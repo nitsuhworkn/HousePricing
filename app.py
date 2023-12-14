7a5c9eedb3d3ddab30d82da1f33791634acd5a5d
Nitsuhwork Negash <Nitsuhict.gmail.com>
2023-12-14 11:00:35 +0300

    This is the commite for the web application

--- app.py
+++ app.py
@@ -22,6 +22,17 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict',methods=['POST'])    
+def predict():
+    data=[float(x) for x in request.form.values()] 
+    final_input=scalar.tranform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=REGmodel.predict(final_input)[0]
+    render_template("home.html",prediction_text="The Predicted House Price is: {}".format(output))
+ 
+
+
+
 if __name__=="__main__":
     app.run(debug=True)
 
